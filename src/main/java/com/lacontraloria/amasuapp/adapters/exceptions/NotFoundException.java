0902b4d1ebaa7c7063e42408b55cc887b958b655
Gabriel Arrien <garrien1@gmail.com>
2024-07-11 21:26:33 -0300

    Adjusting oracle jdbc driver

---
+++ src/main/java/com/lacontraloria/amasuapp/adapters/exceptions/NotFoundException.java
@@ -0,0 +1,4 @@
+package com.lacontraloria.amasuapp.adapters.exceptions;
+
+public class NotFoundException {
+}
---
+++ src/main/java/com/lacontraloria/amasuapp/services/PersonaUpdateService.java
@@ -0,0 +1,4 @@
+package com.lacontraloria.amasuapp.services;
+
+public class PersonaUpdateService {
+}
